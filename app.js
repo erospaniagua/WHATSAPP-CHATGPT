7670ebb63732295adb5ad125fffcce145335f29e
Eros <eros.gear@gmail.com>
2023-03-04 14:35:38 -0700

    chatcompletion

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const flowChatGPT = addKeyword('ia')
       await CHATGPT.runCompletion(message).then(result => {
         console.log (message);
         return flowDynamic(result)
+
       });      
     })
 
@@ -30,3 +31,36 @@ const main = async () => {
 }
 
 main()
+
+// const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+// const QRPortalWeb = require('@bot-whatsapp/portal')
+// const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+// const MockAdapter = require('@bot-whatsapp/database/mock')
+// const CHATGPT = require('./chatgpt')
+
+// const flowChatGPT = addKeyword('ia')
+//     .addAnswer('Preguntame lo que quieras',{capture:true}, async (ctx, {flowDynamic}) => {
+//         var message = ctx.body;
+//       await CHATGPT.runCompletion(message).then(result => {
+//         console.log (message);
+//         return flowDynamic(result)
+//       });      
+//     })
+
+
+// const main = async () => {
+//     const adapterDB = new MockAdapter()
+//     const adapterFlow = createFlow([flowChatGPT])
+//     const adapterProvider = createProvider(BaileysProvider)
+
+//     createBot({
+//         flow: adapterFlow,
+//         provider: adapterProvider,
+//         database: adapterDB,
+//     })
+
+//     QRPortalWeb()
+// }
+
+// main()
--- chatgpt.js
+++ chatgpt.js
@@ -6,31 +6,27 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-async function runCompletion (message) {
-    // const { from, body } = message;
-    // const queue=[]
-    // const completion = await openai.sendMessage(body,{
-    //     conversationId: !queue.length 
-    //     ? undefined 
-    //     :queue[queue.length - 1].conversationId,
-    //     perentMessageId: !queue.length
-    //     ? undefined 
-    //     :queue[queue.length - 1].id,
-    // });
-
-    // queue.push(completion);
-
-    //     const parseMesage = {
-    //         ...completion,
-    //         awnser: completion, text,
-    //     };
-    //     sendFlowSimple([parseMesage], from)
-    const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: message,
-        max_tokens: 200,
-    });
-    return completion.data.choices[0].text;
+async function runCompletion (message){
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{"role": "system", "content": "You are a helpful assistant that makes a lot of jokes."},
+                   {"role": "user", "content": message}
+                  ],
+      });
+      console.log(completion.data.choices[0].message);
+      return completion.data.choices[0].message.content;
+    
 }
-
 module.exports={runCompletion}
+
+// async function runCompletion (message) {
+
+//     const completion = await openai.createCompletion({
+//         model: "text-davinci-003",
+//         prompt: message,
+//         max_tokens: 400,
+//     });
+//     return completion.data.choices[0].text;
+// }
+
+// module.exports={runCompletion}
